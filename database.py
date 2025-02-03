c86aaadc5dc3a24ea4c014288eb4771d5feb8d40
kimminki10 <pangshe10@gmail.com>
2025-02-03 16:37:31 +0900

    fix: add azure global ssl certificate

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,11 +6,14 @@ from config import get_settings
 
 settings = get_settings()
 
+SSL_CERT_PATH = os.path.join(os.getcwd(), "DigiCertGlobalRootCA.crt.pem")
 SQLALCHEMY_DATABASE_URL = (
     "mysql+mysqldb://"
     f"{settings.database_username}:{settings.database_password}"
     f"@{settings.database_host}/{settings.database_name}"
+    "?ssl_ca=" + SSL_CERT_PATH
 )
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
